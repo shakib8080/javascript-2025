adba75ea09559a810b65357e801af86129d3ac1d
shakib8080 <mdshakibulhasan0808@gmail.com>
2025-01-05 12:34:08 +0600

    practice-firt-commit

---
+++ pactice.js
@@ -0,0 +1,137 @@
+/* const myMoney = 1000;
+let orangePrice = 280;
+let mangoPrice = 490;
+let totallCost = orangePrice + mangoPrice;
+let rturn = myMoney - totallCost;
+console.log(rturn);
+
+let amount = 119;
+let person = 5;
+let remainder = amount % person;
+console.log(remainder); */
+/* let subject = 8;
+let bangla = 75;
+let english = 25;
+let biology = 65;
+let chemistry = 80;
+let physics = 35;
+let mathmatics = 45;
+let grammer = 99;
+let history = 50;
+let totallNumber = 75 + 25 + 65 + 80 + 35 + 45 + 99 + 50;
+let avarage = totallNumber / subject;
+
+console.log(parseFloat(avarage)); */
+
+// comparison
+/* let ashik = 50;
+let shakib = 80;
+if (ashik <= shakib) {
+  console.log("shakib hasan ");
+} else {
+  console.log("shakib");
+} */
+/* let salary = 25000;
+let havesCar = true;
+let havesHouse = true;
+let qualificationBcs = false; */
+
+/* if (salary > 20000) {
+  console.log("i am interested  ");
+} else {
+  console.log("i am not interested ");
+} */
+
+/* if (
+  (salary > 30000 && qualificationBcs == true) ||
+  havesCar == false ||
+  havesHouse == false
+) {
+  console.log("i am inerested ");
+} else if ((havesCar == true && qualificationBcs == true) || havesCar == true) {
+  console.log("ami khabo ");
+} else {
+  console.log("i am not interested ");
+} */
+
+/* let disprice = 5000;
+if (disprice < 6000) {
+  let dicount = (disprice * 10) / 100;
+  let discountPrice = disprice - dicount;
+  console.log(discountPrice);
+} else {
+  console.log("we are exreamly sorry ");
+} */
+
+/* let age = 10;
+let price = 500;
+if (age < 17) {
+  console.log("you can eat for free");
+} else if (age > 60) {
+  // discount 10 %
+  const discount = (price * 10) / 100;
+  const payAmount = price - discount;
+  console.log(payAmount);
+} */
+
+// const money = 95;
+
+// if (money >= 200) {
+//   console.log("ami khabo ");
+// } else {
+//   if (money > 100) {
+//     console.log("ami kutti ");
+//   } else {
+//     if (money > 50) {
+//       console.log("ami small");
+//     }
+//   }
+// }
+
+// let age = 20;
+// let money = 500;
+// if (age < 21) {
+//   // discount 10 %
+//   let dicount = (money * 10) / 100;
+//   let payAmount = money - dicount;
+//   console.log(payAmount);
+// } else if (age < 50) {
+//   // dicount 5%
+//   let secDiscount = (money * 5) / 100;
+//   let secPayamount = money - secDiscount;
+//   console.log(secPayamount);
+// }
+
+/* let price = 500;
+let naneShakib = false;
+price = naneShakib = true
+  ? price > 1000
+    ? price / 2
+    : (price = price + 100)
+  : price + 100;
+console.log(price); */
+/* price = price > 200 ? (price = price + 100) : (price = price - 200);
+console.log(price);
+
+let havesCar = true;
+havesCar = true ? "hudfdh" : "uruudfud";
+console.log(havesCar); */
+
+/* let name = ["shakib", "rakib", "hasan", "masum "];
+console.log(name);
+name[0] = "kasem";
+console.log(name);
+name.push("billal");
+console.log(name);
+name.pop();
+console.log(name);
+name.unshift("ritu");
+console.log(name);
+console.log(name[2]);
+ */
+
+/* let names = ["shakib", "rakib", "hasan", "masum "];
+console.log(names.indexOf("rakib"));
+console.log(names[3]);
+names[1] = "kasem";
+console.log(names); */
